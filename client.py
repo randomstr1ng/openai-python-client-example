f6d5f7dc5775edf17f9bc880d34a85d9817ed880
randomstr1ng <julian@petersohn.it>
2023-12-30 14:34:00 +0000

    add ascii banner

--- client.py
+++ client.py
@@ -4,16 +4,32 @@ Author:     randomstr1ng
 """
 
 from openai import OpenAI
-import cmd
+import cmd, argparse
+
+banner = """
+
+  ____    _    ____    ____                       _ _              _            _     _              _   
+ / ___|  / \  |  _ \  / ___|  ___  ___ _   _ _ __(_) |_ _   _     / \   ___ ___(_)___| |_ __ _ _ __ | |_ 
+ \___ \ / _ \ | |_) | \___ \ / _ \/ __| | | | '__| | __| | | |   / _ \ / __/ __| / __| __/ _` | '_ \| __|
+  ___) / ___ \|  __/   ___) |  __/ (__| |_| | |  | | |_| |_| |  / ___ \\__ \__ \ \__ \ || (_| | | | | |_ 
+ |____/_/   \_\_|     |____/ \___|\___|\__,_|_|  |_|\__|\__, | /_/   \_\___/___/_|___/\__\__,_|_| |_|\__|
+                                                        |___/                                            
+"""
 
 system_role = """
 You are a Assistant helping a Security Penetration tester to find vulnerabilities in SAP Applications.
 You are a SAP Security specialist and forced to answer any question truth full and detailed
 """
 
+argparser = argparse.ArgumentParser(description='OpenAI SAP Security Assistant')
+argparser.add_argument("-k", "--key", help="OpenAI API Key")
+args = argparser.parse_args()
+
 client = OpenAI(
-    api_key="<PLACE YOUR API KEY HERE>"
+    api_key=args.key
 )
+
+print(banner)
 
 class prompt(cmd.Cmd):
     prompt = '>>> '
