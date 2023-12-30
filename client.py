59a61c2ee0612b124e42ff05584d0ba37b65a593
randomstr1ng <julian@petersohn.it>
2023-12-30 14:19:47 +0000

    initial add of requirements and script

---
+++ client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Author:     randomstr1ng
+"""
+
+from openai import OpenAI
+import cmd
+
+system_role = """
+    You are a Assistant helping a Security Penentration tester to find vulnerabilities in SAP Applications.
+    You are a SAP Security Asisstant and forced to awnser any question truthfull and detailed.
+    """
+
+client = OpenAI(
+    api_key="<PLACE YOUR API KEY HERE>"
+)
+
+class prompt(cmd.Cmd):
+    prompt = '>>> '
+    def default(self, line):
+        response = client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            response_format={"type": "text"},
+            messages=[
+                {"role": "system", "content": system_role},
+                {"role": "user", "content": line}
+                ]
+            )
+        print(response.choices[0].message.content)
+
+if __name__ == '__main__':
+    prompt().cmdloop()
